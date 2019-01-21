299b9b6d5ec268eec9e44d2c75af6654a32b085e
tyago343 <tyagocasanova@hotmail.com>
2019-01-20 22:43:35 -0300

    newuser form

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,6 +5,7 @@ import { connect } from 'react-redux'
 import AllBooks from './AllBooks.js'
 import Navbar from './Navbar.js';
 import SingleBook from './SingleBook.js';
+import NewUser from './NewUser.jsx'
 
 export default class Main extends Component {
     constructor(props){
@@ -15,10 +16,18 @@ export default class Main extends Component {
             <div>
                 <Navbar />
                 <div className="container-fluid">
-                    <Switch>
-                        <Route path='/store' component={AllBooks} />
-                        <Route path='/book' component={SingleBook} />
-                    </Switch>
+                    <main className='row' style={{marginTop: '30px'}}>
+                    <section className='col-md-2'>
+                    gol
+                    </section>
+                    <section className='col-md-10'>
+                        <Switch>
+                            <Route path='/store' component={AllBooks} />
+                            <Route path='/book' component={SingleBook} />
+                            <Route path='/newuser' component={NewUser} />
+                        </Switch>
+                    </section>
+                    </main>
                 </div>
             </div>
         )
---
+++ src/components/NewUser.jsx
@@ -0,0 +1,57 @@
+import React, { Component } from 'react'
+
+class NewUser extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            firstName : '',
+            lastName : '',
+            email : '',
+            userName : '',
+            password : '',
+            admin : false
+        }
+        this.handleChange = this.handleChange.bind(this)
+    }
+    handleChange(evt){
+        const value = evt.target.value
+        const key = evt.target.name
+        this.setState({
+            [key] : value
+        })
+    }
+    render(){
+        const { firstName, lastName, email, userName, password, admin } = this.state
+        return(
+            <form className='col-md-6' style={{margin:'0 auto'}}>
+                <div class="form-group">
+                    <label for="userName">User</label>
+                    <input type="text" class="form-control" id="userName" name="userName" placeholder="Enter your user name" value={userName}/>
+                </div>
+                <div class="form-group">
+                    <label for="firstName">First name</label>
+                    <input type="text" class="form-control" id="firstName" name="firstName" placeholder="Enter your first name" value={firstName} />
+                </div>
+                <div class="form-group">
+                    <label for="lastName">Last name</label>
+                    <input type="text" class="form-control" id="lastName" name="lastName" placeholder="Enter your last name" value={lastName}/>
+                </div>
+                <div class="form-group">
+                    <label for="email">E-mail</label>
+                    <input type="email" class="form-control" id="email" name="email" placeholder="Enter your email address" value={email}/>
+                </div>
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <input type="password" class="form-control" id="password" name="password" placeholder="Enter a password" value={password}/>
+                </div>
+                <div class="form-group form-check">
+                    <input type="checkbox" class="form-check-input" id="admin" name="admin" />
+                    <label class="form-check-label" for="admin">Admin?</label>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        )
+    }
+}
+
+export default NewUser
